0dd662e61e01f088e81106d5ade119683237e4d6
Floodlock <36305507+floodlock@users.noreply.github.com>
2019-05-31 12:51:01 +0100

    new permission link

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client,message,args) => {
     var embed = new Discord.RichEmbed()
         	.setTitle("Add me")        	
-        	.addField("Want to add me to your server?", "use this invite [link](https://discordapp.com/oauth2/authorize?client_id=420907324945989632&scope=bot&permissions=8)")
+        	.addField("Want to add me to your server?", "use this invite [link](https://discordapp.com/api/oauth2/authorize?client_id=420907324945989632&permissions=1610018294&scope=bot)")
     message.channel.send(embed)
 }
 
