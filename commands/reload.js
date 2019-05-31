2df69ac5d0c7d6ec72942c42363abf5ba1fe82ac
Lachlan <ItsLachy@users.noreply.github.com>
2019-05-30 22:40:00 -0400

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,10 +1,12 @@
 const discord = require("discord.js")
 
 module.exports.run = async (client,message,args) => {
-	if(!message.author.id === "165811958828761089") message.reply("you do not have permission to reload commands")
-	else{
-	message.reply("reloadig all commands")
+	if(!message.author.id === "165811958828761089") 
+	message.reply(":white_check_mark: | Reloading all commands")
 	loadcmd()
+	else{
+	message.reply(":x: | You are not authorized to use this command.")
+
 }
 }
 
