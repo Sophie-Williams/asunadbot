11aa6928c85585c345b2fdc4f44b45b8c9efb3ea
Lachlan <ItsLachy@users.noreply.github.com>
2019-05-31 07:40:41 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (client,message,args) => {
-     message.channel.send("PONG! API Latency is " + Math.round(client.ping) + "ms");
+     message.channel.send("**PONG!** API Latency is " + Math.round(client.ping) + "ms");
 }
 
 module.exports.help = {
